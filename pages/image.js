255ac68f90c15a80e35f6faf4d18bfc8aa45fd24
Sudirman <46485543+dirman8@users.noreply.github.com>
2023-06-15 16:53:41 +0700

    bugging

--- pages/image.js
+++ pages/image.js
@@ -1,20 +1,19 @@
 import Image from "next/image";
-import sky from "../public/sky.jpg"
+import iphone from "../public/sky.jpg";
 import styles from "../styles/Fill.module.css";
 
 const Fill = () => (
-  <div className={styles.container}>
-   <Image 
-   alt="sample-foto" 
-   src={sky}
-    contain
-    sizes="100%" 
-    style=
-    {{
-      objectFit: "none",
-    }}
-    />
-  </div>
+	<div className={styles.container}>
+		<Image
+			alt="sample-foto"
+			src={iphone}
+			contain
+			sizes="100%"
+			style={{
+				objectFit: "none",
+			}}
+		/>
+	</div>
 );
 
-export default Fill
+export default Fill;
